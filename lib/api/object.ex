f8e109ff1fb72957e0ec0e7c2b61f81ada89579a
Hilder Santos <hilder@hildersantos.com>
2017-04-28 02:54:13 -0300

    Documentation updated

--- lib/api/object.ex
+++ lib/api/object.ex
@@ -1,6 +1,10 @@
 defmodule S3Analyst.Api.Object do
 	alias S3Analyst.Api.Object
 
+	@moduledoc """
+	`%S3Analyst.Api.Object{}` struct.
+	"""
+
 	defstruct key: nil, last_modified: nil, size: nil, storage_class: nil
 	@type t :: %Object{key: String.t, last_modified: any, size: number, storage_class: String.t}
 
--- lib/utils/table_formatter.ex
+++ lib/utils/table_formatter.ex
@@ -1,6 +1,17 @@
 defmodule S3Analyst.Utils.TableFormatter do
+	alias S3Analyst.Api.Bucket
+
 	@headers [name: "Name", creation_date: "Creation Date", total_files: "Total Files", total_size: "Total Size", last_modified_date: "Last Modified Date", region: "Region"]
 
+	@moduledoc """
+	Module responsible for table output.
+	"""
+
+	@doc """
+	Prints a formatted table. Keep in mind that it requires valid headers and a `%Bucket{}` list
+	to work.
+	"""
+	@spec print_table([%Bucket{}], Keyword.t) :: String.t
 	def print_table(buckets, headers \\ @headers) do
 		with columns <- columns_data(buckets, headers),
 			columns_width <- calc_width(columns),
@@ -18,13 +29,13 @@ defmodule S3Analyst.Utils.TableFormatter do
 
 			IO.puts(separator(columns_width))
 		end
-		# Pego os itens das rows com as headers (keys)
-		# Gero uma nova lista contendo uma lista com várias listas (cada uma, uma row)
-		# Faço os cálculos necessários pra printar o resultado (normalizado)
 	end
 
+	# Below, there's some private methods to organize the table info.
+
+	# This one extracts and sorts table data in columns
 	defp columns_data(buckets, headers) do
-		buckets =  [Enum.into(headers, %{}) | buckets]
+		buckets = [Enum.into(headers, %{}) | buckets]
 		for {header_key, _header_value} <- headers do
 			for bucket <- buckets do
 				Map.get(bucket, header_key)
@@ -33,26 +44,31 @@ defmodule S3Analyst.Utils.TableFormatter do
 		end
 	end
 
+	# Calculates each column width based on text length
 	defp calc_width(columns) do
 		for column <- columns, do: column |> Enum.map(&String.length/1) |> Enum.max
 	end
 
+	# Formats a column based on its width
 	defp format_column(columns_width) do
 		"| " <> Enum.map_join(columns_width, " | ", fn width ->
 			"~-#{width}s"
 		end) <> " | " <> "~n"
 	end
 
+	# Returns a row separator
 	defp separator(columns_width) do
 		"--" <> Enum.map_join(columns_width, "-+-", fn width ->
 			List.duplicate("-", width)
 		end) <> "--"
 	end
 
+	# Prints one row based on data and format
 	defp print_one_row(data, format) do
 		:io.format(format, data)
 	end
 
+	# Prints all rows
 	defp print_rows(data, format) do
 		data
 		|> List.zip
@@ -61,6 +77,7 @@ defmodule S3Analyst.Utils.TableFormatter do
 		|> Enum.each(&print_one_row(&1, format))
 	end
 
+	# Normalizes data (converts into binary data)
 	defp normalize_data(data) when is_binary(data) do
 		data
 	end
